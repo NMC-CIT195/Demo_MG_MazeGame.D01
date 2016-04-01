ea236e525691adaa442cedda28098c65eee62244
John Velis <john@johnvelis.com>
2016-04-01 15:44:32 -0400

    Initial Commit

---
+++ Demo_MG_MazeGame.D01.Starter/MazeGame.cs
@@ -0,0 +1,200 @@
+﻿using System; // add to allow Windows message box
+using System.Runtime.InteropServices; // add to allow Windows message box
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using System.Collections.Generic;
+
+namespace Demo_MG_MazeGame
+{
+    /// <summary>
+    /// enumeration of all possible game actions
+    /// </summary>
+    // TODO 06 - Add GameAction enums
+    public enum GameAction
+    {
+        None,
+        PlayerRight,
+        PlayerLeft,
+        Quit
+    }
+
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MazeGame : Game
+    {
+        // add code to allow Windows message boxes when running in a Windows environment
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern uint MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        // set the cell size in pixels
+        private const int CELL_WIDTH = 64;
+        private const int CELL_HEIGHT = 64;
+
+        // set the map size in cells
+        private const int MAP_CELL_ROW_COUNT = 8;
+        private const int MAP_CELL_COLUMN_COUNT = 10;
+
+        // set the window size
+        private const int WINDOW_WIDTH = MAP_CELL_COLUMN_COUNT * CELL_WIDTH;
+        private const int WINDOW_HEIGHT = MAP_CELL_ROW_COUNT * CELL_HEIGHT;
+
+        // wall objects
+        // TODO 01 - Add two walls variables
+
+
+        // player object
+        // TODO 02 - Add a player variable
+
+
+        // variable to hold the player's current game action
+        // TODO 07 - Add variable to store current GameAction
+
+
+        // keyboard state objects to track a single keyboard press
+        // TODO 10 - Add variables to handle key state
+
+
+        // declare a GraphicsDeviceManager object
+        GraphicsDeviceManager graphics;
+
+        // declare a SpriteBatch object
+        SpriteBatch spriteBatch;
+
+        public MazeGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+
+            // set the window size 
+            graphics.PreferredBackBufferWidth = MAP_CELL_COLUMN_COUNT * CELL_WIDTH;
+            graphics.PreferredBackBufferHeight = MAP_CELL_ROW_COUNT * CELL_HEIGHT;
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // add floors, walls, and ceilings
+            // TODO 03a - Add walls to map
+
+
+            // add the player
+            // TODO 04a - Add player to map
+
+
+            // set the player's initial speed
+            // TODO 05 - Set players initial speed
+
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Note: wall and player sprites loaded when instantiated
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // get the player's current action based on a keyboard event
+            // TODO 11 - Get player's new GameAction
+
+
+            // TODO 12 - Process player's GameAction using a switch statement
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            // TODO 03b - Draw walls on map
+
+
+            // TODO 04b - Draw player on map
+
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// get keyboard events
+        /// </summary>
+        /// <returns>GameAction</returns>
+        // TODO 08 - Create method to handle keyboard events
+        private GameAction GetKeyboardEvents()
+        {
+            GameAction playerGameAction = GameAction.None;
+
+
+            return playerGameAction;
+        }
+
+        /// <summary>
+        /// check the current state of the keyboard against the previous state
+        /// </summary>
+        /// <param name="theKey">bool new key press</param>
+        /// <returns></returns>
+        // TODO 09 - Create method to handle key state
+        private bool CheckKey(Keys theKey)
+        {
+            // allows the key to be held down
+            //return newState.IsKeyDown(theKey);
+
+            // player must continue to tap the key
+            //return oldState.IsKeyDown(theKey) && newState.IsKeyUp(theKey); 
+
+            return true;
+        }
+
+        /// <summary>
+        /// test for player collision with a wall object
+        /// </summary>
+        /// <param name="wall">wall object to test</param>
+        /// <returns>true if collision</returns>
+        // TODO 13 - Test for collision with a wall
+        private bool PlayerHitWall(Wall wall)
+        {
+            return true;
+        }
+    }
+}
